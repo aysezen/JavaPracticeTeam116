61f0d425a890830ba6100bfe06b83401fbf1040e
Sezen <aysezen6@gmail.com>
2023-03-31 10:56:35 +0200

    first commit

---
+++ src/day01/Q3_Print_Println.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class Q3_Print_Println {
+
+    public static void main(String[] args) {
+
+        // println tum satiri isgal eder
+        // print ise sadece karakter sayisi kadar alani isgal eder
+
+        System.out.print("Ayse");
+        System.out.print("Sule");
+        System.out.println("Yusuf");
+        System.out.println("Bilal Fuat");
+        System.out.print("Elif");
+        System.out.print("\n");
+        System.out.print("Sevda");
+
+    }
+
+}
+
+
